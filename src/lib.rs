6ad87fd9abef5b4723fc43caf3a26fa3167d0343
mamg22 <45301823+mamg22@users.noreply.github.com>
2024-12-08 12:43:08 -0400

    Add utility for parsing text grids

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use std::env;
 use std::fs;
 
+use vector::Vector2d;
+
 pub mod direction;
 pub mod grid;
 pub mod vector;
@@ -11,4 +13,16 @@ pub fn load_input() -> String {
     let input_str = fs::read_to_string(input_filename).expect("Could not load input file");
 
     input_str
+}
+
+pub fn grid_indices<'grid>(
+    grid_str: &'grid str,
+) -> Box<dyn Iterator<Item = (Vector2d<usize>, char)> + 'grid> {
+    let width = grid_str.lines().next().expect("Non empty grid").len();
+    Box::new(
+        grid_str
+            .char_indices()
+            .filter(|ch_idx| ch_idx.1 != '\n')
+            .map(move |(idx, ch)| (Vector2d::new(idx % width, idx / width), ch)),
+    )
 }
